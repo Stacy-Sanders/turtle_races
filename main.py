874f6e6d1febc0fb07f66c19fb413630bf5da324
Stacy-Sanders <stacy.sanders081@gmail.com>
2023-01-09 14:17:39 -0500

    move excess code to scratch file, add to do

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import turtle
 from turtle import Turtle, Screen
 from random import randint
 
@@ -9,21 +8,7 @@ screen.setup(width=500, height=400)  # height and width text not needed, added f
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 user_bet = screen.textinput(title="Make your bet", prompt=f"{colors}\nWhich turtle will win the race?\nChoose a color:")
 
-# tim = Turtle(shape="turtle")
-# tim.color("green")
-#
-# purple = Turtle(shape="turtle")
-# purple.color("purple")
-# blue = Turtle(shape="turtle")
-# blue.color("blue")
-# orange = Turtle(shape="turtle")
-# orange.color("orange")
-# red = Turtle(shape="turtle")
-# red.color("red")
-# yellow = Turtle(shape="turtle")
-# yellow.color("yellow")
-#
-# turtle_racers = [tim, purple, blue, orange, red, yellow]
+
 turtle_racers = []
 y = 150
 # for turtle in turtle_racers:
@@ -49,15 +34,11 @@ while is_race_on:
                 print(f"You've won! The {winning_color} turtle is the winner!")
             else:
                 print(f"You lost. The {winning_color} turtle is the winner.")
-            print(list(winners))
+            # print(list(winners))
         turtle.forward(randint(0, 10))
 
 
 screen.exitonclick()
 
+# TODO: 1. Work on edge case where 2nd turtle passes x = 225 in same loop
 
-#  for turtle_index in range(0, 6):
-#      tim = Turtle(shape="turtle")
-#      tim.color[colors[turtle_index]]
-#      tim.penup()
-#      tim.goto(x=-230, y=-100)
